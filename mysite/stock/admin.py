96cde2193670c48d3c2eb31dece7882797ac58a5
latewind <lsqwell@sina.com>
2017-11-28 19:39:04 +0800

    update

--- mysite/stock/admin.py
+++ mysite/stock/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']})
+    ]
 
 
 # Register your models here.
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
--- mysite/stock/views.py
+++ mysite/stock/views.py
@@ -1,27 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import Http404
-from django.http import  HttpResponse
-from .models import  Question
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+from .models import Question, Choice
+from django.db.models import F
 
 
-# Create your views here.
-def index(request):
-    latest_question_list = Question.objects.order_by('_pub_date')
-    context = {"latest_question_list": latest_question_list}
-    return render(request, 'stock/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'stock/index.html'
+    context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not")
-    return HttpResponse("You're looking at question %s." % question_id)
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')
 
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'stock/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'stock/results.html'
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'stock/results.html', context={"question": question})
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'stock/detail.html', {'question': question, 'error_message': "You Don't selected a choic"})
+    else:
+        selected_choice.votes = F('votes') + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('stock:results', args=(question.id,)))
