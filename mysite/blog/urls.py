07d44a4fdcadc135ab390e1fe5f907627cea1545
latewind <lsqwell@sina.com>
2017-12-06 23:48:44 +0800

    add form

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
 from . import views
 
+
+app_name = 'blog'
 urlpatterns = [
     url(r'^$', views.PostListView.as_view(), name='post_list'),
     # url(r'^$', views.post_list, name='post_list'),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<post>[-\w]+)/$', views.post_detail, name='post_detail')
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<post>[-\w]+)/$', views.post_detail, name='post_detail'),
+    url(r'^(?P<post_id>\d+)/share/$', views.post_share,
+        name='post_share')
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView, DetailView
+from .forms import EmailPostForm
 
 
 class PostListView(ListView):
@@ -39,3 +40,16 @@ def post_detail(request, year, month, day, post):
                              publish__day=day)
     return render(request, 'blog/post/detail.html',
                   {'post': post})
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+    cd = None
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.changed_data
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html',
+                  {'post': post, 'form': form, 'cd': cd})
