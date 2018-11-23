39925c034bf4025e62b129bce7285d9897fefb8b
latewind <lsqwell@sina.com>
2018-11-24 00:23:39 +0800

    u

--- Server/windServer.py
+++ Server/windServer.py
@@ -3,20 +3,22 @@
 from http.server import BaseHTTPRequestHandler
 import socketserver
 
+
 class RequestHandler(BaseHTTPRequestHandler):
+
     def do_GET(self):
         self.send_response(200)
         self.send_header("Content-Type", "text/html")
         self.send_header("Content-Length", str(len(self.Page)))
         self.end_headers()
         page = self.create_page()
-        self.wfile.write(bytes(page,encoding='utf-8'))
+        self.wfile.write(bytes(page, encoding='utf-8'))
 
     def create_page(self):
         values = {
-        'date_time'         :       self.date_time_string(),
-        'client_address'    :       self.client_address[0],
-        'path'              :       self.path
+            'date_time': self.date_time_string(),
+            'client_address': self.client_address[0],
+            'path': self.path
         }
         return self.Page.format(**values)
 
@@ -31,7 +33,8 @@ class RequestHandler(BaseHTTPRequestHandler):
 </html>
 '''
 
+
 if __name__ == '__main__':
     address = ('', 18887)
-    with socketserver.TCPServer(address,RequestHandler) as httpd:
+    with socketserver.TCPServer(address, RequestHandler) as httpd:
         httpd.serve_forever()
