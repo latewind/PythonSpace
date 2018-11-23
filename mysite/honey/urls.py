42673520f5a264d2c4196f825f64d2f65146a371
latewind <lsqwell@sina.com>
2018-11-24 07:37:48 +0800

    mv

--- mysite/honey/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-app_name = 'honey'
-urlpatterns = [
-    url(r'^$', views.HomePageView.as_view(), name='home'),
-    url(r'^test$', views.TestPageView.as_view(), name='test'),
-]
