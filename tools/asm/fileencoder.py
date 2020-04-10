87f59866fca4b9b694d0bac2199868a2df8659ec
latewind <lsqwell@sina.com>
2020-04-10 23:02:11 +0800

    mir2消息监控程序

--- tools/asm/fileencoder.py
+++ tools/asm/fileencoder.py
@@ -1,12 +1,14 @@
 import tkinter as tk
 from tkinter.scrolledtext import ScrolledText
 from tkinter.constants import END, LEFT, BOTH
+from tkinter import ttk
 
 
-def hex_decode(hex_str):
+def hex_decode(hex_str, encoding):
     print(hex_str)
     b = bytes.fromhex(hex_str.strip())
-    return b.decode(encoding='GB18030', errors="ignore")
+
+    return b.decode(encoding=encoding, errors="ignore")
 
 
 class Application(tk.Frame):
@@ -17,6 +19,10 @@ class Application(tk.Frame):
         self.create_widgets()
 
     def create_widgets(self):
+        self.combbox = ttk.Combobox(self, values=["utf-8", 'gbk', 'GB18030', 'utf-16'])
+        self.combbox.set('GB18030')
+        self.combbox.pack(side="top")
+
         self.text = ScrolledText(self)
         self.text.pack(side="top")
 
@@ -34,9 +40,10 @@ class Application(tk.Frame):
 
     def say_hi(self):
         a = self.text.get('0.0', 'end')
-        text_str = hex_decode(a)
-        self.stext.insert('1.0', text_str)
 
+        text_str = hex_decode(a, self.combbox.get())
+
+        self.stext.insert('1.0', text_str)
 
 
 root = tk.Tk()
