92ad0d8519b4818b0a97485f01f41b586c558dec
latewind <lsqwell@sina.com>
2020-04-12 14:58:01 +0800

    消息分段拼接

--- tools/mir2/scannet.py
+++ tools/mir2/scannet.py
@@ -3,15 +3,12 @@ import time
 from monsterdata import MonsterDropHistory, session
 import datetime
 
-KEY_HEADER = 'ddccbbaa4a000000100006f76400d4ff00000100'
+KEY_HEADER = 'ddccbbaa'
 SYSTEM_TEXT = 'a1becfb5cdb3a1bf'
 PATTERN = r'【系统】\[(?P<monster_name>.*?)\]在\[(?P<map_name>.*?)\]被击杀，掉落了 \{\[(?P<object_name>.*?)\]'
-
-
-def save_pkg():
-    dpkt = sniff(count=20, iface='WLAN', filter="src host 43.227.222.97 port 7333")  # 这里是针对单网卡的机子，多网卡的可以在参数中指定网卡
-
-    wrpcap("demo.pcap", dpkt)
+# }.
+KEY_TAIL = '7d2e'
+partial_list = []
 
 
 def read_pkg():
@@ -26,23 +23,47 @@ def read_pack(pkt):
     print(pkt)
 
 
+# src host 43.227.222.97 7333
 def scan_network():
-    dpkt = sniff(prn=parse_pkg, count=0, iface='WLAN', filter="host 43.227.222.97 7333")  # 这里是针对单网卡的机子，多网卡的可以在参数中指定网卡
+    dpkt = sniff(prn=parse_pkg, count=0, iface='WLAN',
+                 filter="dst host 192.168.1.102 port 52576")  # 这里是针对单网卡的机子，多网卡的可以在参数中指定网卡
 
 
 def parse_pkg(data):
+    global partial_list
     if 'Raw' not in data:
         return
     raw_hex = data['Raw'].load.hex()
 
-    if not raw_hex.startswith('ddccbbaa'):
+    save_to_file(raw_hex, 'raw_hex.log')
+    # 没有消息头，没有消息尾部，再进一步判断
+    if not raw_hex.startswith(KEY_HEADER) and not raw_hex.endswith(KEY_TAIL):
+        # 长度满格，且有缓存的消息头，说明是中间部分，通过，否则return
+        if len(raw_hex) == 1072 and len(partial_list) > 0:
+            pass
+        else:
+            return
+
+    # 有消息头，没消息尾部，部分1，存起来
+    if raw_hex.startswith(KEY_HEADER) and not raw_hex.endswith(KEY_TAIL):
+        partial_list.append(raw_hex)
         return
+    # 没消息头，没消息尾部，中间部分，存起来
+    if not raw_hex.startswith(KEY_HEADER) and not raw_hex.endswith(KEY_TAIL):
+        partial_list.append(raw_hex)
+        return
+
+    # 有消息尾，没消息头，partial_list里面有内容,消息尾部
+    if not raw_hex.startswith(KEY_HEADER) and raw_hex.endswith(KEY_TAIL) and len(partial_list) > 0:
+        partial_list.append(raw_hex)
+        raw_hex = ''.join(partial_list)
+        partial_list = []
 
     # 保存原包文标识
     need_save_hex = False
 
     # 根据消息头标识，分割消息
-    msg_list = raw_hex.split('ddccbbaa')
+    msg_list = raw_hex.split(KEY_HEADER)
     for msg in msg_list:
         if len(msg) > 32 and msg[32:].startswith(SYSTEM_TEXT):
             need_save_hex = True
@@ -57,8 +78,8 @@ def parse_pkg(data):
         save_to_file(raw_hex)
 
 
-def save_to_file(format_msg):
-    with open("record.log", mode='a+', encoding='utf-8') as f:
+def save_to_file(format_msg, file_name='record.log'):
+    with open(file_name, mode='a+', encoding='utf-8') as f:
         f.write(format_msg + "\n")
 
 
