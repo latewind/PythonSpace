034e35f6a5792e5c526692c823f514895616475c
latewind <lsqwell@sina.com>
2020-04-12 21:33:26 +0800

    消息分段拼接

--- tools/mir2/scannet.py
+++ tools/mir2/scannet.py
@@ -8,7 +8,9 @@ SYSTEM_TEXT = 'a1becfb5cdb3a1bf'
 PATTERN = r'【系统】\[(?P<monster_name>.*?)\]在\[(?P<map_name>.*?)\]被击杀，掉落了 \{\[(?P<object_name>.*?)\]'
 # }.
 KEY_TAIL = '7d2e'
+LEFT_BRACE = '5b'
 partial_list = []
+partial = False
 
 
 def read_pkg():
@@ -23,7 +25,8 @@ def read_pack(pkt):
     print(pkt)
 
 
-# src host 43.227.222.97 7333
+#
+# dst host 192.168.1.102 port 52576
 def scan_network():
     dpkt = sniff(prn=parse_pkg, count=0, iface='WLAN',
                  filter="dst host 192.168.1.102 port 52576")  # 这里是针对单网卡的机子，多网卡的可以在参数中指定网卡
@@ -31,43 +34,40 @@ def scan_network():
 
 def parse_pkg(data):
     global partial_list
+    global partial
     if 'Raw' not in data:
         return
     raw_hex = data['Raw'].load.hex()
+    save_to_file(raw_hex, 'raw_hex_new.log')
 
-    save_to_file(raw_hex, 'raw_hex.log')
-    # 没有消息头，没有消息尾部，再进一步判断
-    if not raw_hex.startswith(KEY_HEADER) and not raw_hex.endswith(KEY_TAIL):
-        # 长度满格，且有缓存的消息头，说明是中间部分，通过，否则return
-        if len(raw_hex) == 1072 and len(partial_list) > 0:
-            pass
-        else:
-            return
-
-    # 有消息头，没消息尾部，部分1，存起来
-    if raw_hex.startswith(KEY_HEADER) and not raw_hex.endswith(KEY_TAIL):
+    # 分段头部
+    if len(raw_hex) == 1072 and raw_hex.startswith(KEY_HEADER):
+        partial = True
         partial_list.append(raw_hex)
         return
-    # 没消息头，没消息尾部，中间部分，存起来
-    if not raw_hex.startswith(KEY_HEADER) and not raw_hex.endswith(KEY_TAIL):
+    # 分段中间部位
+    if len(raw_hex) == 1072 and partial is True:
         partial_list.append(raw_hex)
         return
-
-    # 有消息尾，没消息头，partial_list里面有内容,消息尾部
-    if not raw_hex.startswith(KEY_HEADER) and raw_hex.endswith(KEY_TAIL) and len(partial_list) > 0:
+    # 分段结尾
+    if len(raw_hex) < 1072 and partial is True:
+        # 结束分段
         partial_list.append(raw_hex)
         raw_hex = ''.join(partial_list)
-        partial_list = []
+        partial_list.clear()
+        partial = False
 
+    # 不是标准消息，返回
+    if not raw_hex.startswith(KEY_HEADER):
+        return
     # 保存原包文标识
     need_save_hex = False
-
-    # 根据消息头标识，分割消息
-    msg_list = raw_hex.split(KEY_HEADER)
+    # 按照 【系统】 分割
+    msg_list = raw_hex.split(SYSTEM_TEXT)
     for msg in msg_list:
-        if len(msg) > 32 and msg[32:].startswith(SYSTEM_TEXT):
+        if len(msg) > 0 and msg.startswith(LEFT_BRACE):
             need_save_hex = True
-            bs = bytes.fromhex(msg[32:])
+            bs = bytes.fromhex(SYSTEM_TEXT + msg)
             msg_ch = bs.decode(encoding='GB18030', errors="ignore")
             format_msg = "{}:{}".format(time.strftime('%Y-%m-%d %H:%M:%S'), msg_ch)
             print(format_msg)
